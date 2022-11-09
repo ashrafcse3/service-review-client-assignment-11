63dc37d70f8b1a5d769cc61158c286bebc14cd67
Ashraful Alam <ashrafcse3@gmail.com>
2022-11-09 12:06:41 +0000

    review have been added to the mongodb

--- src/Pages/Review/Review.js
+++ src/Pages/Review/Review.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReviewForm from './ReviewForm';
 import ReviewTexts from './ReviewTexts';
 
 const Review = ({ reviews }) => {
+    const [updatedReviews, setUpdatedReviews] = useState(reviews);
+
+    const serviceId = reviews[0].service_id;
+
+    const handleForm = event => {
+        event.preventDefault();
+        const review = event.target.review.value;
+
+        const data = {
+            review: review,
+            service_id: serviceId,
+            user_id: "636ab1406fbf03bf44767445",
+            user_name: "admin1",
+            user_profile_pic: "https://cdn.pixabay.com/photo/2014/03/25/16/32/user-297330_1280.png"
+        };
+
+        fetch('http://localhost:4000/review', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    }
     return (
         <div className='mx-auto max-w-screen-xl mt-9'>
             <h1 className='text-3xl font-bold'>Reviews for this service</h1>
@@ -11,12 +39,12 @@ const Review = ({ reviews }) => {
                 Reviewd by <span className='font-semibold'>{reviews.length} User</span>
             </div>
             {
-                reviews.map(review => <ReviewTexts
+                updatedReviews.map(review => <ReviewTexts
                     key={review._id}
                     review={review}
                 ></ReviewTexts>)
             }
-            <ReviewForm></ReviewForm>
+            <ReviewForm handleForm={handleForm}></ReviewForm>
         </div>
     );
 };
--- src/Pages/Review/ReviewForm.js
+++ src/Pages/Review/ReviewForm.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-const ReviewForm = () => {
+const ReviewForm = ({ handleForm }) => {
+    //fetch the user details and put it on this data
+    // const user
+
+
+
     return (
         <div className='rounded-md px-7 py-5 bg-white text-lg mb-8'>
             <h1 className='text-2xl font-bold'>Write a review about this service</h1>
-            <form className='mt-9'>
+            <form onSubmit={handleForm} className='mt-9'>
                 <textarea name="review" id="" rows='5' className="border rounded block p-2.5 w-full text-sm" placeholder='Write your review'></textarea>
                 <input type="text" className='block w-full border my-3' readOnly placeholder='Your name? admin' />
                 <button className='btn'>Submit review</button>
